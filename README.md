16aef1cda97d9e343f1fb829d424ce7facf3a0f1
Jamesbarford <jamesbarfordevans@gmail.com>
2020-09-15 18:25:55 +0100

    readme and update to build.sh

---
+++ README.md
@@ -0,0 +1,5 @@
+## T-9 predictive text implementation
+
+### To build 👷‍♂️
+
+In the root run: `sh ./build.sh` and the app will be available on `localhost:3000`
--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ CLIENT=dist-client
 rm -rf ./$SERVER ./$CLIENT;
 cd ./server || exit;
 npm run build;
-cp package.json ../$SERVER && cd ../$SERVER && npm install;
+cp package.json ../$SERVER && cp ./App/lib/dictionary.txt ../$SERVER/lib && cd ../$SERVER && npm install;
 cd ../client && npm run build;
 cd ../ && node ./$SERVER;
