cc5294116eaf1c42b24a2db30229ee4e74b37e40
Jamesbarford <jamesbarfordevans@gmail.com>
2020-09-15 18:00:39 +0100

    better error handling

--- client/lib/apiRequests/request.ts
+++ client/lib/apiRequests/request.ts
@@ -1,11 +1,9 @@
-import { createError } from "./RequestState";
-
 export async function makeGetRequest(url: string): Promise<any> {
     const response = await fetch(`/api${url}`);
 
     try {
         return await response.json();
     } catch (e) {
-        createError("Failed to make request");
+        throw e;
     }
 }
